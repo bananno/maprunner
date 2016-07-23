95af8f2ad42c791d721d7fdd9b9f0f6a74301a1c
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2016-07-23 14:20:38 -0700

    Change variable names

--- designmaze.js
+++ designmaze.js
@@ -4,9 +4,9 @@ function designMaze() {
     addWall(10, 10, 1, 80);
 }
 
-function addWall(rowStart, columnStart, rowSize, columnSize) {
-   for(var row = rowStart; row < (rowStart + rowSize); row++) {
-        for(var column = columnStart; column < (columnStart + columnSize); column++) {
+function addWall(rowStart, columnStart, width, height) {
+	for(var row = rowStart; row < (rowStart + width); row++) {
+        for(var column = columnStart; column < (columnStart + height); column++) {
             world[row][column] = 1;
         }
     }
